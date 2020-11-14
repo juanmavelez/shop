b4f49a7e52627512fdcd85386fcf34900908685f
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2020-11-14 22:04:35 +0100

    aplying smart-dum patron into prodcuts, changin products.component for products.container and moving it inot a container folder

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './components/products/products.component';
+import { ProductsContainer } from './containers/products/products.container';
 import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductRoutingModule } from './product-routing.module';
 import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [ProductComponent, ProductsComponent, ProductDetailComponent],
+  declarations: [ProductComponent, ProductsContainer, ProductDetailComponent],
   imports: [ProductRoutingModule, CommonModule, MaterialModule],
 })
 export class ProductModule {}
